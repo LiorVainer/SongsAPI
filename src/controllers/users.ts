ae190cfa40b14f608284c5ae2aac1912ca661c72
Lior <lior.vainer@agamimp.net>
2021-08-29 17:39:16 +0300

    29/8/2021: Work

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,17 +1,54 @@
 import { Router } from 'express';
 import { ObjectId } from 'mongodb';
-import { getTable } from '../dal';
+import { getCollection } from '../dal';
+import {
+  checkUserDoesNotExist,
+  registerCredentialsValitation,
+} from '../middlewares/validation/users';
+import { handlerGuard } from '../middlewares/handlerGuard';
+import { jwtUserIdSign } from '../utils/jwt';
 
 const router = Router();
 
-const users = getTable('users');
+const users = getCollection('users');
 
-router.get('', async (req, res) => {
-  res.send(await users.find({}).toArray());
-});
-
-router.get(':id', async (req, res) => {
+router.get('/:id', async (req, res) => {
   res.send(await users.findOne({ _id: new ObjectId(req.params.id) }));
 });
+
+router.post(
+  '/register',
+  registerCredentialsValitation,
+  checkUserDoesNotExist,
+  handlerGuard(async (req, res) => {
+    const { email, password, name, type } = req.body;
+
+    let userId;
+
+    if (type === 'admin') {
+      userId = await (
+        await users.insertOne({
+          email: email,
+          password: password,
+          name: name,
+          type: 'admin',
+        })
+      ).insertedId.toHexString();
+    } else {
+      userId = await (
+        await users.insertOne({
+          email: email,
+          password: password,
+          name: name,
+          type: 'admin',
+        })
+      ).insertedId.toHexString();
+    }
+
+    return res.send({ secretKey: jwtUserIdSign(userId) });
+  })
+);
+
+router.post('/login', (req, res) => {});
 
 export default router;
